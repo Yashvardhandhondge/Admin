769d5d5016aa88d4ab8a7b73dc67720d51dd3858
adev07 <adityaanand1245@gmail.com>
2025-01-08 13:30:50 +0530

    initial commit

---
+++ src/components/admin-panel/sidebar.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { Menu } from "@/components/admin-panel/menu";
+import { SidebarToggle } from "@/components/admin-panel/sidebar-toggle";
+import { useSidebar } from "@/hooks/use-sidebar";
+import { useStore } from "@/hooks/use-store";
+import { cn } from "@/lib/utils";
+import Nixtour from "@/assets/svg/nixtour_logo.svg"
+import Image from "next/image";
+
+export function Sidebar() {
+  const sidebar = useStore(useSidebar, (x) => x);
+  if (!sidebar) return null;
+  const { isOpen, toggleOpen, getOpenState, setIsHover, settings } = sidebar;
+  return (
+    <aside
+      className={cn(
+        "fixed top-0 left-0 z-20 h-screen -translate-x-full lg:translate-x-0 transition-[width] ease-in-out duration-300",
+        !getOpenState() ? "w-[90px]" : "w-72",
+        settings.disabled && "hidden"
+      )}
+    >
+      <SidebarToggle isOpen={isOpen} setIsOpen={toggleOpen} />
+      <div
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+        className="relative flex flex-col px-3 py-4 shadow-md dark:shadow-zinc-800"
+      >
+        <div className="flex items-center justify-center">
+          <Image width={120} height={10} src={Nixtour} alt="" />
+        </div>
+        <Menu isOpen={getOpenState()} />
+      </div>
+    </aside>
+  );
+}
